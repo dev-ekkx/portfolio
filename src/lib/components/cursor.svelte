65724a8df9027a39deb67d09b614851797fae530
Emmanuel Kpendo <devekkx@gmail.com>
2025-05-23 07:52:09 +0000

    chore: create a custom cursor icon

---
+++ src/lib/components/cursor.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let x = $state(0);
+	let y = $state(0);
+	onMount(() => {
+		const handleMouseMove = (event: MouseEvent) => {
+			x = event.clientX;
+			y = event.clientY;
+		};
+
+		window.addEventListener('mousemove', handleMouseMove);
+
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+		};
+	});
+</script>
+
+<div class="cursor" style="transform: translate({x}px, {y}px);">
+	<i class="fa-solid fa-arrow-pointer scale-150 text-white"></i>
+</div>
+
+<style>
+	.cursor {
+		position: fixed;
+		pointer-events: none; /* Prevent cursor from interfering with clicks */
+		transform: translate(-50%, -50%);
+		z-index: 9999;
+	}
+</style>
