e0af8933f717fb4b7766432fd0f9b4bb12c4899a
Emmanuel Kpendo <56701505+dev-ekkx@users.noreply.github.com>
2025-05-27 07:29:08 +0000

    chore: create a custom cursor icon (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import CustomCursor from '$lib/ui-elements/cursor.svelte';
+	import Snowflakes from '$lib/ui-elements/snowflakes.svelte';
+</script>
+
+<svelte:head>
+	<title>DR</title>
+	<meta
+		content="Emmanuel Kpendo, Dela Ricch, front-end, frontend software engineer, solutions architect"
+		name="description"
+	/>
+</svelte:head>
+
+<CustomCursor />
+<Snowflakes />
