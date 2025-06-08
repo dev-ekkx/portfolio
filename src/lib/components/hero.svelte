390b8c3c3a8094776dbdda463a22be099d1db089
Emmanuel Kpendo <devekkx@gmail.com>
2025-06-08 14:55:55 +0000

    feature: update cta buttons

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -8,12 +8,16 @@
 			<span>engineer</span>
 		</span>
 
-		<p>Hi! I'm <b>Emmanuel Kpendo.</b> A creative Frontend Developer with 3+ years of experience in building
+		<p>Hi! I'm <b>Emmanuel Kpendo.</b> A creative (Software & Cloud) Engineer with 3+ years of experience in building
 			high-performance, scalable, and responsive applications.</p>
 
 		<div class="flex items-center gap-4 mt-4">
-			<button class="border px-4 py-1">Let's talk</button>
-			<button class="border px-4 py-1 flex items-center gap-2">
+			<button
+				class="border px-6 py-1 bg-primary border-primary rounded-2xl w-[9rem] hover:bg-transparent hover:!text-primary transition-all">
+				Let's talk
+			</button>
+			<button
+				class="border px-4 py-1 flex items-center gap-2 rounded-2xl w-[9rem] hover:border-primary hover:!text-primary transition-all">
 				<span>
 				Get Resume
 				</span>
